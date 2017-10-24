79bc99d565d2ef23788073da21c8b5e458d19a5a
Lex Yehia <lex.yehia@icloud.com>
2017-10-24 12:43:40 -0400

    Small refactoring

--- app.js
+++ app.js
@@ -16,20 +16,32 @@ const connectDb = new Promise((resolve, reject) => {
     }) 
 })
 
-const main = (name) => {
+const printRows = (name, result) => {
+    if (!result) {
+        return console.log('No results found.')
+    }
+
+    console.log(`Found ${result.rows.length} person(s) by the name '${name}':`)
+    result.rows.forEach((e, i) => {
+        console.log(`- ${i+1}: ${e.first_name} ${e.last_name}, born ${e.birthdate.toLocaleDateString()}`)
+    })
+}
+
+const findRows = (name) => {
     connectDb.then(client => {
         client.query("SELECT * FROM famous_people WHERE last_name = ($1) OR first_name = ($1)", [name])
         .then(result => {
             if (result.rows.length === 0) {
-                return console.log("No results found.")
+                printRows(name, null)
             } else {
-                console.log(`Found ${result.rows.length} person(s) by the name '${name}':`)
-                result.rows.forEach((e, i) => {
-                    console.log(`- ${i+1}: ${e.first_name} ${e.last_name}, born ${e.birthdate.toLocaleDateString()}`)
-                })
+                printRows(name, result)
             }
         })
     })
+}
+
+const main = (name) => {
+    findRows(name)
 }
 
 main(process.argv[2])
